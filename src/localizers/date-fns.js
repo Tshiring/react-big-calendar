57d28065616fe63d35ef32e0b88c67c589be0b79
Tshiring <tshiring1814@gmail.com>
2024-08-04 13:57:17 +0545

    fix: changes made in date fns

--- src/localizers/date-fns.js
+++ src/localizers/date-fns.js
@@ -1,78 +1,66 @@
-import * as dates from '../utils/dates'
 import { DateLocalizer } from '../localizer'
-import { formatInTimeZone, fromZonedTime, toZonedTime } from 'date-fns-tz'
-// import {
-//   // format,
-//   formatInTimeZone,
-//   utcToZonedTime,
-//   zonedTimeToUtc,
-// } from 'date-fns-tz'
-// import { startOfWeek, getDay } from 'date-fns'
+import * as dates from '../utils/dates'
 
-let dateRangeFormat = ({ start, end }, culture, local) =>
+const dateRangeFormat = ({ start, end }, culture, local) =>
   `${local.format(start, 'P', culture)} – ${local.format(end, 'P', culture)}`
 
-let timeRangeFormat = ({ start, end }, culture, local) =>
+const timeRangeFormat = ({ start, end }, culture, local) =>
   `${local.format(start, 'p', culture)} – ${local.format(end, 'p', culture)}`
 
-let timeRangeStartFormat = ({ start }, culture, local) =>
+const timeRangeStartFormat = ({ start }, culture, local) =>
   `${local.format(start, 'h:mma', culture)} – `
 
-let timeRangeEndFormat = ({ end }, culture, local) =>
+const timeRangeEndFormat = ({ end }, culture, local) =>
   ` – ${local.format(end, 'h:mma', culture)}`
 
-let weekRangeFormat = ({ start, end }, culture, local) =>
+const weekRangeFormat = ({ start, end }, culture, local) =>
   `${local.format(start, 'MMMM dd', culture)} – ${local.format(
     end,
     dates.eq(start, end, 'month') ? 'dd' : 'MMMM dd',
     culture
   )}`
 
-export let formats = {
+const formats = {
   dateFormat: 'dd',
   dayFormat: 'dd eee',
   weekdayFormat: 'cccc',
+
   selectRangeFormat: timeRangeFormat,
   eventTimeRangeFormat: timeRangeFormat,
   eventTimeRangeStartFormat: timeRangeStartFormat,
   eventTimeRangeEndFormat: timeRangeEndFormat,
+
   timeGutterFormat: 'p',
+
   monthHeaderFormat: 'MMMM yyyy',
   dayHeaderFormat: 'cccc MMM dd',
   dayRangeHeaderFormat: weekRangeFormat,
   agendaHeaderFormat: dateRangeFormat,
+
   agendaDateFormat: 'ccc MMM dd',
   agendaTimeFormat: 'p',
   agendaTimeRangeFormat: timeRangeFormat,
 }
 
-const dateFnsTzLocalizer = function ({
+const dateFnsTzLocalizer = ({
   startOfWeek,
   getDay,
   format: _format,
   locales,
-}) {
+  timeZone,
+  formatTz,
+  utcToZonedTime,
+}) => {
   return new DateLocalizer({
     formats,
     firstOfWeek(culture) {
       return getDay(startOfWeek(new Date(), { locale: locales[culture] }))
     },
-    format(value, formatString, culture, timezone) {
-      const date = timezone ? toZonedTime(value, timezone) : new Date(value)
-      return timezone
-        ? formatInTimeZone(date, timezone, formatString, {
-            locale: locales[culture],
-          })
-        : _format(date, formatString, { locale: locales[culture] })
-    },
-    getCurrentTimezone() {
-      return Intl.DateTimeFormat().resolvedOptions().timeZone
-    },
-    convertToLocalTime(date, timezone) {
-      return toZonedTime(date, timezone)
-    },
-    convertToUTC(date, timezone) {
-      return fromZonedTime(date, timezone)
+    format(value, formatString, culture) {
+      const zonedDate = utcToZonedTime(value, timeZone)
+      return formatTz(zonedDate, formatString, {
+        locale: locales[culture],
+      })
     },
   })
 }
